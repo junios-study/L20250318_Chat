f6001b275da54576d4b5de9aba68437640b5ca54
junios-study <junios@naver.com>
2025-03-18 16:43:18 +0900

    custom packet 만들기

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,21 +11,39 @@ namespace Client
 {
     class Program
     {
+
+        //[][]
         static void Main(string[] args)
         {
+            string jsonString = "{\"message\" : \"fdsdfsdfdsfdsfsdfdsfsdf sdfgsdfsdfg.\"}";
+            byte[] message = Encoding.UTF8.GetBytes(jsonString);
+            ushort length = (ushort)message.Length;
+            //길이  자료
+            //[][] [][][][][][][][]
+            byte[] lengthBuffer = new byte[2];
+            lengthBuffer = BitConverter.GetBytes(length);
+
+            //[][][][][][][][][][][]
+            byte[] buffer = new byte[2 + length];
+
+            Buffer.BlockCopy(lengthBuffer, 0, buffer, 0, 2);
+            Buffer.BlockCopy(message, 0, buffer, 2, length);
+
             Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
 
             clientSocket.Connect(listenEndPoint);
 
-            string jsonString = "{\"message\" : \"안녕하세요2\"}";
-            byte[] message = Encoding.UTF8.GetBytes(jsonString);
-            int SendLength = clientSocket.Send(message);
+            int SendLength = clientSocket.Send(buffer, buffer.Length, SocketFlags.None);
 
-            byte[] buffer = new byte[1024];
-            int RecvLength = clientSocket.Receive(buffer);
-            string JsonString = Encoding.UTF8.GetString(buffer);
+            int RecvLength = clientSocket.Receive(lengthBuffer, 2, SocketFlags.None);
+            length = BitConverter.ToUInt16(lengthBuffer, 0);
+
+            byte[] recvBuffer = new byte[4096];
+            RecvLength = clientSocket.Receive(recvBuffer, length, SocketFlags.None);
+
+            string JsonString = Encoding.UTF8.GetString(recvBuffer);
 
             Console.WriteLine(JsonString);
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -30,21 +30,38 @@ namespace Server
 
             Socket clientSocket = listenSocket.Accept();
 
-            byte[] buffer = new byte[1024];
-            int RecvLength = clientSocket.Receive(buffer);
+            //[][] [][][][][][]
 
-            string JsonString = Encoding.UTF8.GetString(buffer);
+            //패킷 길이 받기(header)
+            byte[] headerBuffer = new byte[2];
+            int RecvLength = clientSocket.Receive(headerBuffer, 2, SocketFlags.None);
+            ushort packetlength = BitConverter.ToUInt16(headerBuffer, 0);
 
-            JObject json = JObject.Parse(JsonString);
-            //JsonConvert.DeserializeObject<Message>(JsonString);
-            if (json.Value<string>("message").ToString().CompareTo("안녕하세요") == 0)
-            {
-                byte[] message;
-                JObject result = new JObject();
-                result.Add("message", "반가워");
-                message =  Encoding.UTF8.GetBytes(result.ToString());
-                int SendLength = clientSocket.Send(message);
-            }
+            //[][][][][]
+            //실제 패킷(header 길이 만큼)
+            byte[] dataBuffer = new byte[4096];
+            RecvLength = clientSocket.Receive(dataBuffer, packetlength, SocketFlags.None);
+
+            string JsonString = Encoding.UTF8.GetString(dataBuffer);
+
+            Console.WriteLine(JsonString);
+
+            //Custom 패킷 만들기
+            //다시 전송 메세지
+            string message = "{ \"message\" : \"sdfdsfdsfsdfdsdsfds 잘 받았다.\"}";
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            ushort length = (ushort)messageBuffer.Length;
+            //길이  자료
+            //[][] [][][][][][][][]
+            headerBuffer = BitConverter.GetBytes(length);
+
+            //[][][][][][][][][][][]
+            byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+
+            Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+            Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, length);
+
+            int SendLength = clientSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
 
 
             clientSocket.Close();
