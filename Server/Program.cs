d4aea24d83f3c1aba9970c00bd94eadc2d71b15c
junios-study <junios@naver.com>
2025-03-25 15:19:07 +0900

    저장 안됨

--- Server/Program.cs
+++ Server/Program.cs
@@ -95,12 +95,15 @@ namespace Server
                                     result.Add("messge", "success");
                                     result.Add("name", dataReader["name"].ToString());
                                     result.Add("email", dataReader["email"].ToString());
-                                    result.ToString();
-
+                                    SendPacket(clientSocket, result.ToString());
                                 }
                                 else
                                 {
                                     //로그인 실패
+                                    JObject result = new JObject();
+                                    result.Add("code", "loginresult");
+                                    result.Add("messge", "failed");
+                                    SendPacket(clientSocket, result.ToString());
                                 }
 
 
@@ -125,6 +128,10 @@ namespace Server
                                 mySqlCommand2.ExecuteNonQuery();
 
                                 //가입 성공했습니다.
+                                JObject result = new JObject();
+                                result.Add("code", "signupresult");
+                                result.Add("messge", "success");
+                                SendPacket(clientSocket, result.ToString());
                             }
                         }
                         catch(Exception e)
@@ -192,6 +199,21 @@ namespace Server
                     return;
                 }
             }
+        }
+
+        static void SendPacket(Socket toSocket, string message)
+        {
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
+
+            byte[] headerBuffer = BitConverter.GetBytes(length);
+
+            byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+            Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+            Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+
+            int SendLength = toSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+
         }
 
 
