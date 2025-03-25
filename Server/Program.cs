60b3f17ee95a15e1ed9599373fd0045acb326cc5
junios-study <junios@naver.com>
2025-03-25 15:07:28 +0900

    로그인 서버로 변경

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
@@ -32,6 +35,7 @@ namespace Server
                 Console.WriteLine($"Connect client : {clientSocket.RemoteEndPoint}");
 
                 Thread workThread = new Thread(new ParameterizedThreadStart(WorkThread));
+
                 workThread.IsBackground = true;
                 workThread.Start(clientSocket);
                 //threadManager.Add(workThread);
@@ -59,23 +63,44 @@ namespace Server
                         string JsonString = Encoding.UTF8.GetString(dataBuffer);
                         Console.WriteLine(JsonString);
 
+                        string connectionString = "server=localhost;user=root;database=users;password=qweasd123";
+                        MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+
                         JObject clientData = JObject.Parse(JsonString);
+                        string code = clientData.Value<String>("code");
 
-                        string message = "{ \"message\" : \"" + clientData.Value<String>("message") + "\"}";
-                        byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
-                        ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
-
-                        headerBuffer = BitConverter.GetBytes(length);
-
-                        byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
-                        Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
-                        Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
-                        lock (_lock)
+                        try
                         {
-                            foreach (Socket sendSocket in clientSockets)
+                            if (code.CompareTo("Login") == 0)
                             {
-                                int SendLength = sendSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+                                mySqlConnection.Open();
+                                MySqlCommand mySqlCommand = new MySqlCommand();
+                                mySqlCommand.Connection = mySqlConnection;
+
+                                mySqlCommand.CommandText = "select * from users where user_id = @user_id and user_password = @user_password";
+                                mySqlCommand.Prepare();
+                                mySqlCommand.Parameters.AddWithValue("@user_id", "htk008");
+                                mySqlCommand.Parameters.AddWithValue("@user_password", "1234");
+
+                                MySqlDataReader dataReader = mySqlCommand.ExecuteReader();
                             }
+                            else if (code.CompareTo("Signup") == 0)
+                            {
+                                MySqlCommand mySqlCommand2 = new MySqlCommand();
+                                mySqlCommand2.Connection = mySqlConnection;
+
+                                mySqlCommand2.CommandText = "insert into users (user_id, user_password, name, email) values ( @user_id, @user_password, @name, @email)";
+                                mySqlCommand2.Prepare();
+                                mySqlCommand2.Parameters.AddWithValue("@user_id", "abc002");
+                                mySqlCommand2.Parameters.AddWithValue("@user_password", "333");
+                                mySqlCommand2.Parameters.AddWithValue("@name", "노형민");
+                                mySqlCommand2.Parameters.AddWithValue("@email", "abc001@abc001.com");
+                                mySqlCommand2.ExecuteNonQuery();
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message);
                         }
                     }
                     else
@@ -147,14 +172,20 @@ namespace Server
 
             listenSocket.Listen(10);
 
-            Thread acceptThread = new Thread(new ThreadStart(AcceptThread));
-            acceptThread.IsBackground = true;
-            acceptThread.Start();
+            Task acceptTask = new Task(AcceptThread);
+            //Thread acceptThread = new Thread(new ThreadStart(AcceptThread));
+            //acceptThread.IsBackground = true;
+            //acceptThread.Start();
+            acceptTask.Start();
 
-            acceptThread.Join();
-
+            //acceptThread.Join();
+            acceptTask.Wait();
 
             listenSocket.Close();
+
+            //FileStream fs = new FileStream("test.txt", FileMode.Open);
+            //byte[] buffer = new byte[1024];
+            //Task<int> result = fs.ReadAsync(buffer, 0, 1024);
         }
     }
 }
